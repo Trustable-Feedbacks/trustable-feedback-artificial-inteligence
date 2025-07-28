eea96f8b3687d3cb6a8c3ae893f6195e030cdfee
eduardocostaam <eduardocosta942z@gmail.com>
2025-07-28 17:12:35 -0300

    train.py and checkpoint at utils

--- train.py
+++ train.py
@@ -1,6 +1,37 @@
-# Algorithm:
-# Create the Batches: Batch_size, dataset, Shuffle
-# Define: Epochs, Embedding Dimension, RNN Units, Loss Function, LR and etc...
-# Acess Checkpoint
-# Compile the model
-# Train
+import tensorflow as tf
+import os
+from keras.callbacks import ModelCheckpoint
+from preprocess import PADDED_TEXT_SEQUENCE, ENCODED_LABEL_GRADE, ENCODED_LABEL_FAIR
+from model import MODEL
+
+# Define training dataset:
+text_tensor = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(PADDED_TEXT_SEQUENCE))
+grade_tensor = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(ENCODED_LABEL_GRADE))
+lables_tensor = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(ENCODED_LABEL_FAIR))
+
+    # Zip datasets:
+dataset = tf.data.Dataset.zip(((text_tensor, grade_tensor), lables_tensor))
+
+
+# Create Batchs:
+BATCH_SIZE = 64
+EPOCHS = 10
+BUFFER_SIZE = 10000
+
+data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# Create Checkpoint:
+CHECKPOINT_DIR = './utils/'
+CHECKPOINT_PREFIX = os.path.join(CHECKPOINT_DIR, 'trustable_feedbacks_ai_callback')
+
+checkpoint_callback = ModelCheckpoint(
+    filepath=CHECKPOINT_PREFIX + '.h5',
+    save_best_only=True,
+    monitor='loss',
+    mode='max',
+    save_freq='epoch',
+    save_weights_only=True
+)
+
+# Training:
+history = MODEL.fit(data, epochs=EPOCHS, callbacks=[checkpoint_callback])
